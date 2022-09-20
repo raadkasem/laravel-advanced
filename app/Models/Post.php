8eda881ae092c06734e6a03e78557047e9ddaa5f
raadkasem <rkasem0@gmail.com>
2022-09-20 16:02:32 +0300

    One To One Morph Relation between Image and Post, Image and User

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+
+    /**
+     * @return MorphOne
+     */
+    public function image(): MorphOne
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
